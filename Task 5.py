a40b35b5a9fe2f56b911a26da1434d4421059321
sarthakikanchangire21-ux <sarthakikanchangire21@gmail.com>
2026-06-28 18:14:36 +0530

    This is List Tasks

---
+++ Task 5.py
@@ -0,0 +1,8 @@
+# Lacky Number Collector
+number = []
+for i in range(5):
+    num = int(input("Enter a number: "))
+    number.append(num)
+    print("Lucky Numbers: ")
+    for num in number:
+        print(num)
